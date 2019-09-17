a784549dad29442baf056e47f84ae2f786fb16dd
leeminseok <ems9928@naver.com>
2019-09-17 13:44:29 +0900

    free

--- UBIstudy/src/main/java/ubi/model/UbiDao.java
+++ UBIstudy/src/main/java/ubi/model/UbiDao.java
@@ -1,8 +1,14 @@
 package ubi.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import utility.Paging;
 
 @Component("myUbiDao")
 public class UbiDao {
@@ -11,5 +17,57 @@ public class UbiDao {
 	@Autowired
 	SqlSessionTemplate sqlSessionTemplate;
 	
-
+	public int GetTotalCount(Map<String, String> map) {
+		int cnt = -1;
+		cnt = sqlSessionTemplate.selectOne(namespace + ".GetTotalCount", map);
+		return cnt;
+	}
+	
+	public List<UbiBean> selectAll(Paging pageInfo, Map<String, String> map){
+		List<UbiBean> lists=new ArrayList<UbiBean>();
+		lists = sqlSessionTemplate.selectList(namespace + ".selectAll", map);
+		try {
+			System.out.println("dao : "+lists.get(3).getTitle());			
+		}catch(Exception e) {System.out.println("dao실패");}
+		return lists;
+	}
+	
+	public void updateReadCount(int id) {
+		sqlSessionTemplate.update(namespace + ".updateReadCount", id);
+	}
+	
+	public void freeUpdate(int id,String name,String title,String content) {
+		UbiBean bean=new UbiBean(0, 0, 0, 0,"0","0","0","0","0");
+		bean.setNum(id);
+		bean.setName(name);
+		bean.setTitle(title);
+		bean.setContent(content);
+		sqlSessionTemplate.update(namespace + ".FreeUpdate", bean);
+	}
+	public void freeRef(int id,String name,String title,String content,int ref,int re_level,String pw) {
+		UbiBean bean=new UbiBean(0, 0, 0, 0,"0","0","0","0","0");
+		bean.setNum(id);
+		bean.setName(name);
+		bean.setTitle(title);
+		bean.setContent(content);
+		bean.setRef(ref);
+		bean.setRe_level(re_level);
+		bean.setPw(pw);
+		sqlSessionTemplate.insert(namespace + ".FreeRef", bean);
+	}
+	public void freeInsert(int id,String name,String title,String content,String pw) {
+		UbiBean bean=new UbiBean(0, 0, 0, 0,"0","0","0","0","0");
+		bean.setNum(id);
+		bean.setName(name);
+		bean.setTitle(title);
+		bean.setContent(content);
+		bean.setPw(pw);
+		sqlSessionTemplate.insert(namespace + ".FreeInsert", bean);
+	}
+	
+	public void freeDelete(int id) {
+		sqlSessionTemplate.delete(namespace + ".FreeDelete", id);
+	}
+	
+	
 }
